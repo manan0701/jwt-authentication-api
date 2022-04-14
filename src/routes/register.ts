d64d92150a0462345594c3791f7d73f1321c31b3
Manan Patel <pate0658@algonquinlive.com>
2022-04-14 11:27:00 -0400

    feat: add register route

---
+++ src/routes/register.ts
@@ -0,0 +1,60 @@
+import bcrypt from 'bcrypt';
+import 'dotenv/config';
+import { Request, Response } from 'express';
+import { User, UserType } from '../model/user';
+import { generateJwtAccessTokenForUser } from './utils';
+
+const register = async (req: Request, res: Response) => {
+  const user = parseUserInfoFromRequest(req);
+
+  if (!user) {
+    return res
+      .status(400)
+      .send(
+        'Sufficient information not found for user registration. Please provide valid first name, last name, unique username, and password.'
+      );
+  }
+
+  if (await doesUserExist(user)) {
+    return res
+      .status(409)
+      .send('User with the given username already exist. Please login instead.');
+  }
+
+  const persistentUser = await registerUser(user);
+  const accessToken = generateJwtAccessTokenForUser(persistentUser);
+
+  res.cookie('jwt', accessToken, { secure: true, httpOnly: true });
+  res.status(201).send();
+};
+
+const parseUserInfoFromRequest = (request: Request): UserType | null => {
+  if (!request || !request.body) {
+    return null;
+  }
+
+  const { first_name, last_name, username, password } = request.body;
+
+  if (!(first_name && last_name && username && password)) {
+    return null;
+  }
+
+  return { firstName: first_name, lastName: last_name, username: username, password: password };
+};
+
+const doesUserExist = async (user: UserType): Promise<boolean> => {
+  return !!(await User.findOne({ username: user.username }));
+};
+
+const registerUser = async (user: UserType): Promise<UserType> => {
+  const encryptedPassword = await bcrypt.hash(user.password, 10);
+
+  return await User.create({
+    first_name: user.firstName,
+    last_name: user.lastName,
+    username: user.username,
+    password: encryptedPassword,
+  });
+};
+
+export default register;
