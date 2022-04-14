5a0e4796dd5fa32652088e6eaf861f3186303009
Manan Patel <pate0658@algonquinlive.com>
2022-04-13 22:56:29 -0400

    feat: add auth verification middleware

---
+++ src/middleware/auth.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+
+
+const verifyAccessToken = (req: Request, res: Response, next: NextFunction) => {
+    const accessToken = req.cookies.jwt;
+
+    if (!accessToken) {
+        return res.status(403).send('JWT access token cookie not found.');
+    }
+
+    const { ACCESS_TOKEN_KEY = '' } = process.env;
+
+    try {
+        jwt.verify(accessToken, ACCESS_TOKEN_KEY);
+        next();
+    } catch (e) {
+        return res.status(401).send("Failed to authenticate user with the given JWT cookie.");
+    }
+};
+
+export default verifyAccessToken;
