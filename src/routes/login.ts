8b19a658053f1a1b9cdcb0c0d7bc764d23cc7ab4
Manan Patel <pate0658@algonquinlive.com>
2022-04-24 11:11:25 -0400

    feat: add login info validation to login route

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -1,16 +1,18 @@
 import bcrypt from 'bcrypt';
 import { Request, Response } from 'express';
+import { ValidationError } from 'joi';
 import { generateJWTTokensForUser } from '../jwt';
-import { findUserByUsername, UserType } from '../model/user';
+import { findUserByUsername, LoginSchema, UserType } from '../model/user';
 import { persistUserRefreshToken } from '../tokenStore';
 
 const login = async (req: Request, res: Response) => {
-  const { username, password } = req.body;
+  const validationError = validateRequest(req);
 
-  if (!(username && password)) {
-    return res.status(400).send('Valid username and password for the user is required for login.');
+  if (validationError) {
+    return res.status(400).send(`Failed to login the user: ${validationError.message}`);
   }
 
+  const { username, password } = parseLoginInfoFromRequest(req);
   const user: UserType | null = await findUserByUsername(username);
 
   if (!(user && (await bcrypt.compare(password, user.password)))) {
@@ -22,6 +24,20 @@ const login = async (req: Request, res: Response) => {
 
   res.cookie('jwt', accessToken, { secure: true, httpOnly: true });
   res.status(200).send();
+};
+
+const validateRequest = (request: Request): ValidationError | null => {
+  if (!request || !request.body) {
+    return null;
+  }
+  const { username, password } = request.body;
+  const { error } = LoginSchema.validate({ username, password });
+  return error ? error : null;
+};
+
+const parseLoginInfoFromRequest = (request: Request): { username: string; password: string } => {
+  const { username, password } = request.body;
+  return { username, password };
 };
 
 export default login;
