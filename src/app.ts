247f5f5b7eadca557194303c90d146d6376c9b8d
Manan Patel <pate0658@algonquinlive.com>
2022-04-23 23:04:52 -0400

    feat: add refresh token endpoint

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,10 @@ import cookieParser from 'cookie-parser';
 import 'dotenv/config';
 import express from 'express';
 import { connect as dbConnect } from './config/database';
-import verifyAccessToken from './middleware/auth';
+import authenticate from './middleware/auth';
 import home from './routes/home';
 import login from './routes/login';
+import refresh from './routes/refresh';
 import register from './routes/register';
 
 dbConnect();
@@ -16,6 +17,7 @@ app.use(cookieParser());
 
 app.post('/register', register);
 app.post('/login', login);
-app.get('/home', verifyAccessToken, home);
+app.post('/refresh', refresh);
+app.get('/home', authenticate, home);
 
 export default app;
---
+++ src/routes/refresh.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from 'express';
+import jwt, { TokenExpiredError } from 'jsonwebtoken';
+import { generateAccessToken, UserJwtPayload, verifyAccessToken } from '../jwt';
+import { getTokenForUser } from '../tokenStore';
+
+const refresh = async (req: Request, res: Response) => {
+  const accessToken = req.cookies.jwt;
+
+  if (!accessToken) {
+    return res.status(403).send('JWT access token cookie not found.');
+  }
+
+  const payload = jwt.decode(accessToken);
+  const { userId, username } = payload as UserJwtPayload;
+  const refreshToken = getTokenForUser(userId);
+
+  try {
+    verifyAccessToken(refreshToken);
+  } catch (e) {
+    if (e instanceof TokenExpiredError || !refreshToken) {
+      return res.status(401).send('Refresh token is not present or expired. Please login again.');
+    }
+    console.error(e);
+    return res.status(401).send('Failed to generate access token using the refresh token.');
+  }
+
+  const newAccessToken = generateAccessToken(userId, username);
+  res.cookie('jwt', newAccessToken, { secure: true, httpOnly: true });
+  return res.status(200).send(newAccessToken);
+};
+
+export default refresh;
